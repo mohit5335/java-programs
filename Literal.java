c42e4ce05cadd2b7afb0c237a1cc0292bbf40d08
Mohit Sanwal <mohitsanwal53@gmail.com>
2019-03-12 19:34:09 +0530

    Printing Literals in java

---
+++ Literal.java
@@ -0,0 +1,18 @@
+//program to print literals in java
+public class Literal
+{
+public static void main(String...k)
+	{
+		int a = 101;    	 //decimal literal
+		int b = 0100; 		 //octal literal
+		int c = 0xface;		 //hexadecimal literal
+		int d = 0b1111;		 //binary literal
+		String s = "Java";	 //Strings
+		System.out.println("Decimal literal\t"+a);
+		System.out.println("Octal literal\t"+b);
+		System.out.println("Hexadecimal literal\t"+c);
+		System.out.println("Binary literal\t"+d);
+		System.out.println("Strings\t"+s);
+	}
+
+}
