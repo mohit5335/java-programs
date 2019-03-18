917beae856b7f25df553f4a8bfc91b0648472777
Mohit Sanwal <mohitsanwal53@gmail.com>
2019-03-18 18:46:10 +0530

    Bitwise Operators in Java

---
+++ BitwiseOperator.java
@@ -0,0 +1,11 @@
+public class BitwiseOperator 
+{
+	public static void main(String...k)
+	{
+	int a = 30;
+	int b = 43;
+	int c = a & b;		//bitwise and operator
+	int d = a | b;		//bitwise or operator
+	System.out.println("c = "+c+"\nd = "+d);
+	}
+}
