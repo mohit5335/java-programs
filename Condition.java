ee1bc83efb8ae30fc4da0515f239be4950144f36
Mohit Sanwal <mohitsanwal53@gmail.com>
2019-03-18 21:39:34 +0530

    Condition (if and else)

---
+++ Condition.java
@@ -0,0 +1,22 @@
+public class Condition
+{
+	public static void main(String[] arg)
+	{
+		int a,b,c;
+		a = 5;
+		b = 8;
+		c = 3;
+	      	if(a > b && a > c)
+		{
+			System.out.println("A is greater");
+	        }
+		else if(b > c)
+		{
+			System.out.println("B is greater");
+		}
+		else
+		{
+			System.out.println("C is greater");
+		}	
+	}
+}
