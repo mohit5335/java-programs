1d4ae672f5b5aa900ae35f6e19decccc03a0fa18
Mohit Sanwal <mohitsanwal53@gmail.com>
2019-03-20 01:09:56 +0530

    Fibonacci Series

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class FibonacciSeries
+{
+	public static void main(String[] args)
+	{
+		int k,a=1,b=1;
+		Scanner sc = new Scanner(System.in);
+		k=0;
+		System.out.println("Enter the number till you want to search");
+		int n = sc.nextInt();
+		System.out.print("1 1 ");
+		while(k<=n)
+		{
+			k=a+b;
+			if(k>=n)
+			break;
+			System.out.print(k + " ");
+			a=b;
+			b=k;
+		}	
+	}	
+}
