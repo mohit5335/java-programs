0fc97475b40d42f442f10e1a491e9558e43eb47b
Mohit Sanwal <mohitsanwal53@gmail.com>
2019-03-20 00:16:17 +0530

    Print ASCII Values

---
+++ AsciiValue.java
@@ -0,0 +1,10 @@
+public class AsciiValue   	//American Standard Code for Information Interchange
+{
+	public static void main(String...k)
+	{
+		for(int i=1;i<=127;i++)
+		{
+			System.out.printf("%d : %c \n",i,i);
+		}
+	}
+}
