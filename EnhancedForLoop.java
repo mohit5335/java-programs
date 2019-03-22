5f0ab3fb3c23db7fc24232dc39ba8d6b941bf8f7
Mohit Sanwal <mohitsanwal53@gmail.com>
2019-03-22 22:09:44 +0530

    Enhance For Loop

---
+++ EnhancedForLoop.java
@@ -0,0 +1,12 @@
+/* Enhanced for loop is used when we have to fetch all the values
+from any array or collections */
+import java.util.*;
+public class EnhancedForLoop
+{
+	public static void main(String...k)
+	{
+		int values[] = {23,24,25,26,27,28};
+		for(int i : values)
+			System.out.println(i);	
+	}
+}
